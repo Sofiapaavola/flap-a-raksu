91d02df3775896a5c58d057db17a85c9995c3a22
Sofia Paavola <paavola.sofia@gmail.com>
2019-11-14 13:56:21 +0000

    some initial bits

--- main.ts
+++ main.ts
@@ -1,17 +1,13 @@
-const square: HTMLElement = document.getElementById("raksu");
+const square = document.getElementById("raksu");
+let position;
 
 // get raksu to move up and down when i press the button 
-
+// keep the dog in place whilst the background is moving 
 // need to start the game when the user presses the button 
 
 class Raksu { 
     isDead: boolean = false;
     gravity = 0.05; 
-
-    fly() { 
-    
-
-    }
 
     dies() { 
         //when collides
@@ -22,6 +18,17 @@ class Raksu {
     }
 }
 
+
+
+const start = () => { 
+    position = 50;
+    square.style.top = position + "px";
+    document.getElementById("fly").addEventListener("click", fly)
+}
+
+const fly = (distance) => square.style.top = distance + "px";
+
+start();
 
 // pipes 
 
