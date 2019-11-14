00193ab0f3681edb0f1a3d2a8a214037bb9672d8
Sofia Paavola <paavola.sofia@gmail.com>
2019-11-14 13:32:05 +0000

    added raksu and some typescript comments

---
+++ main.ts
@@ -0,0 +1,31 @@
+const square: HTMLElement = document.getElementById("raksu");
+
+// get raksu to move up and down when i press the button 
+
+// need to start the game when the user presses the button 
+
+class Raksu { 
+    isDead: boolean = false;
+    gravity = 0.05; 
+
+    fly() { 
+    
+
+    }
+
+    dies() { 
+        //when collides
+    }
+
+    collides() { 
+        // collides either with a pole or with the ground??
+    }
+}
+
+
+// pipes 
+
+// collision
+
+// call game start
+ 
